01b099a6bc36275a1c995b3ac2ee385007a984b1
Maartenapd0 <172842@student.aventus.nl>
2023-01-18 14:01:03 +0100

    0

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,15 @@
                 font-family: 'Nunito', sans-serif;
             }
         </style>
-    </head>
+    </head><nav>
+  <ul>
+    <li><a href="#home">Home</a></li>
+    <li><a href="#about">Mijn werkpad</a></li>
+    <li><a href="#services">Sertivicaten</a></li>
+    <li><a href="#contact">Toetsen</a></li>
+  </ul>
+</nav>
+
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
