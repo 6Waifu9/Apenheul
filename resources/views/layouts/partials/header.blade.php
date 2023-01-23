191f74e20c0c89049ccd2de0ad177a34816f0694
Maartenapd0 <172842@student.aventus.nl>
2023-01-23 11:34:13 +0100

    apenheul logo

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -3,11 +3,12 @@
     <label class="menu__btn" for="menu__toggle">
         <span></span>
     </label>
-    <ul class="menu__box">
-        <li><a class="menu__item" href="#">Home</a></li>
-        <li><a class="menu__item" href="#">Leer pad</a></li>
-        <li><a class="menu__item" href="#">Toetsen</a></li>
-        <li><a class="menu__item" href="#">Certifaten</a></li>
-        <li><a class="menu__item" href="#">Bestanden</a></li>
+        <ul class="menu__box">
+            <li><img src="../../../../public/img/apenheul.png"></li>
+            <li><a class="menu__item" href="#">Home</a></li>
+            <li><a class="menu__item" href="#">Leer pad</a></li>
+            <li><a class="menu__item" href="#">Toetsen</a></li>
+            <li><a class="menu__item" href="#">Certifaten</a></li>
+            <li><a class="menu__item" href="#">Bestanden</a></li>
     </ul>
 </div>
