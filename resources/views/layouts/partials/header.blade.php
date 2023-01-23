4121d53faecc21af940419e44537c83736a13fe3
6waifu9 <Carlos@speldekamp.nl>
2023-01-23 11:57:58 +0100

    knopjes

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -3,12 +3,28 @@
     <label class="menu__btn" for="menu__toggle">
         <span></span>
     </label>
-        <ul class="menu__box">
-            <li><img src="../../../../public/img/apenheul.png"></li>
-            <li><a class="menu__item" href="#">Home</a></li>
-            <li><a class="menu__item" href="#">Leer pad</a></li>
-            <li><a class="menu__item" href="#">Toetsen</a></li>
-            <li><a class="menu__item" href="#">Certifaten</a></li>
-            <li><a class="menu__item" href="#">Bestanden</a></li>
+    <ul class="menu__box">
+        <li><img src="img/apenheul.svg"></li>
+        <li><a class="menu__item" href="#">Home</a></li>
+        <li><a class="menu__item" href="#">Leer pad</a></li>
+        <li><a class="menu__item" href="#">Toetsen</a></li>
+        <li><a class="menu__item" href="#">Certifaten</a></li>
+        <li><a class="menu__item" href="#">Bestanden</a></li>
     </ul>
-</div>
+    <div
+        class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+        @if (Route::has('login'))
+        <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+            @auth
+            <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
+            @else
+            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+            @if (Route::has('register'))
+            <a href="{{ route('register') }}"
+                class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+            @endif
+            @endauth
+        </div>
+        @endif
+    </div>
